c86f28e9593c31bb9861e77aff8cc7706ab0fc0f
koosco <rnxogud123@naver.com>
2024-09-30 15:19:37 +0900

    refactor: dto record 자료형으로 변경

--- src/main/java/com/goorm/goormweek2/auth/application/dto/TokenDTO.java
+++ src/main/java/com/goorm/goormweek2/auth/application/dto/TokenDTO.java
@@ -1,15 +1,5 @@
 package com.goorm.goormweek2.auth.application.dto;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+public record TokenDTO(String accessToken, String refreshToken) {
 
-@Builder
-@Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class TokenDTO {
-    //구현
 }
