a851a07aece74ed0fa597659650be2314d4fab4a
koosco <rnxogud123@naver.com>
2024-09-30 19:22:35 +0900

    fix: 사용하지 않는 security 설정 변경
    * logout 삭제
    * h2-console 접속을 위한 x-frame 옵션 추가

--- src/main/java/com/goorm/goormweek2/config/security/WebSecurityConfig.java
+++ src/main/java/com/goorm/goormweek2/config/security/WebSecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -34,7 +34,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() throws Exception {
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
 
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
@@ -44,7 +44,7 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
+    public AuthenticationManager authenticationManager() {
         DaoAuthenticationProvider provider = daoAuthenticationProvider();
         provider.setPasswordEncoder(encoder());
         return new ProviderManager(provider);
@@ -57,21 +57,16 @@ public class WebSecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .cors(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable)
+            .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
             .sessionManagement(c ->
                 c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
                     "/webjars/**").permitAll()
                 .requestMatchers("/", "/login", "/register").permitAll()
-                .anyRequest().authenticated())
-            .formLogin(form -> form
-                .loginPage("/login")
-                .defaultSuccessUrl("/", true)
-                .permitAll())
-            .logout(LogoutConfigurer::permitAll)
-            .logout(logout -> logout
-                .logoutUrl("/logout")
-                .permitAll())
+                .anyRequest().permitAll())
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
             .addFilterBefore(new TokenAuthenticationFilter(tokenProvider),
                 UsernamePasswordAuthenticationFilter.class)
             .build();
